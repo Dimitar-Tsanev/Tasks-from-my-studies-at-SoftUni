34fb9c0ed67aa75f5bc11e3475b7c0273e279eb0
Dimitar-Tsanev <Dimitar-Tsanev@users.noreply.github.com>
2024-01-11 13:31:08 +0200

    task 3 done

--- Advanced/Advanced/Stacks and Queues/Lecture/DecimalToBinaryConverter.java
+++ Advanced/Advanced/Stacks and Queues/Lecture/DecimalToBinaryConverter.java
@@ -10,6 +10,7 @@ public class DecimalToBinaryConverter {
         if ( num == 0 ){
             System.out.println ( num );
             return;
+
         }
 
         ArrayDeque <Integer> binary = new ArrayDeque<> ();
