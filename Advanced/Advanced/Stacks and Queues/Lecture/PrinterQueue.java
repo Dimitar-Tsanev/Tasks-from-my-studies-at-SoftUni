57e11e824acb6f079f4c681e3100b3af00ab1ce6
Dimitar-Tsanev <Dimitar-Tsanev@users.noreply.github.com>
2024-01-11 14:02:19 +0200

    task 5 done

--- Advanced/Advanced/Stacks and Queues/Lecture/PrinterQueue.java
+++ Advanced/Advanced/Stacks and Queues/Lecture/PrinterQueue.java
@@ -9,23 +9,23 @@ public class PrinterQueue {
 
         String fileName = scanner.nextLine ();
 
-        while (!"print".equals ( fileName )){
-            if ("cancel".equals ( fileName )){
-                if (queue.isEmpty ()){
+        while (!"print".equals ( fileName )) {
+            if ( "cancel".equals ( fileName ) ) {
+                if ( queue.isEmpty ( ) ) {
                     System.out.println ( "Printer is on standby" );
 
-                }else{
-                    System.out.println ( "Canceled " + queue.poll ( ));
+                } else {
+                    System.out.println ( "Canceled " + queue.poll ( ) );
 
                 }
-            }else{
+            } else {
                 queue.offer ( fileName );
 
             }
             fileName = scanner.nextLine ( );
 
         }
-        queue.forEach ( e -> System.out.println ( queue.poll () ) );
 
+        queue.forEach ( e -> System.out.println ( queue.poll () ) );
     }
 }
