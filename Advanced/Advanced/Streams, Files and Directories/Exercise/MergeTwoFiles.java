89af2439e107b56c4d9ba73ccb40b35e40438a7b
Dimitar-Tsanev <Dimitar-Tsanev@users.noreply.github.com>
2024-01-23 17:51:00 +0200

    task 7 done

--- Advanced/Advanced/Streams, Files and Directories/Exercise/MergeTwoFiles.java
+++ Advanced/Advanced/Streams, Files and Directories/Exercise/MergeTwoFiles.java
@@ -11,8 +11,6 @@ public class MergeTwoFiles {
         Path input2Path = Paths.get ( "C:\\Users\\35988\\Desktop\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\inputTwo.txt" );
         String outputPath = "C:\\Users\\35988\\Desktop\\output.txt";
 
-
-
         try (PrintWriter output = new PrintWriter ( outputPath )){
             List<String> text1 = Files.readAllLines ( input1Path );
             List<String> text2 = Files.readAllLines ( input2Path );
