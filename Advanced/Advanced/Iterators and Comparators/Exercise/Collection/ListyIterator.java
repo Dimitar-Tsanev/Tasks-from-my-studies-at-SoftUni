aa7946590bba690f8cf5598d8683d18c79c525cb
Dimitar-Tsanev <Dimitar-Tsanev@users.noreply.github.com>
2024-02-07 17:45:59 +0200

    task 2 done

---
+++ Advanced/Advanced/Iterators and Comparators/Exercise/Collection/ListyIterator.java
@@ -0,0 +1,60 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ListyIterator<String> implements Iterable<String> {
+    private List<String> data;
+    private int pointer;
+
+    @SafeVarargs
+    public ListyIterator ( String... elements ) {
+
+        this.data = new ArrayList<> ( Arrays.asList ( elements ) );
+
+        this.pointer = 0;
+    }
+    public Iterator<String> iterator () {
+        return new CustomIterator<> ( );
+    }
+
+    @Override
+    public void forEach ( Consumer<? super String> action ) {
+        if (data.isEmpty ( ) ){
+            System.out.println ("Invalid Operation!" );
+        }else{
+            Iterable.super.forEach ( action );
+        }
+    }
+
+    public boolean hasNext() {
+        return this.pointer < this.data.size() - 1;
+    }
+
+    public boolean move () {
+        if ( this.hasNext () ) {
+            this.pointer++;
+            return true;
+        }
+        return false;
+    }
+    public void print () {
+        System.out.println ( !data.isEmpty ( ) ? data.get ( pointer ) : "Invalid Operation!" );
+    }
+
+    private final class CustomIterator<String> implements Iterator<String> {
+        private int counter = 0;
+
+        public boolean hasNext () {
+            return this.counter < data.size ( );
+        }
+
+        public String next () {
+            return (String) data.get ( counter++ );
+        }
+
+    }
+}
