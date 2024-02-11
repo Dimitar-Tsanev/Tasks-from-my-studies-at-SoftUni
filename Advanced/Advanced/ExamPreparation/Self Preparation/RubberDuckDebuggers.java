3d7e6a806268b33b1fbcefe737f612c6688405b1
Dimitar-Tsanev <Dimitar-Tsanev@users.noreply.github.com>
2024-02-11 12:38:52 +0200

    task done

--- Advanced/Advanced/ExamPreparation/Self Preparation/RubberDuckDebuggers.java
+++ Advanced/Advanced/ExamPreparation/Self Preparation/RubberDuckDebuggers.java
@@ -1,6 +1,54 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
+/*Task description - You gathered a few programmers and gave them a
+ task and judging by the time it took them to write the code, you reward them with a type
+ of rubber ducky.
+You will be given two sequences of integers. The first one represents the time it takes a
+programmer to complete a single task. The second one represents the number of tasks you’ve
+given to your programmers.
+Your task is to count how many rubber ducks of each type you’ve given to your programmers.
+While you have values in the sequences, you need to start from the first value of the
+programmers time's sequence and multiply them by the last value of the tasks' sequence:
+
+•	If the calculated time is between any of the time ranges below, you give the
+corresponding ducky and remove the programmer time's value and the tasks' value.
+
+•	If the calculated time goes above the highest range, decrease the number
+ of the task's value by 2. Then, move the programmer time's value to the end
+ of its sequence, and continue with the next operation.
+
+Rubber Ducky type	Time needed to earn it
+Darth Vader Ducky	0 - 60
+Thor Ducky	61 – 120
+Big Blue Rubber Ducky	121 - 180
+Small Yellow Rubber Ducky	181 - 240
+
+Your task is considered done when the sequences are empty.
+
+Input
+•	The first line will represent each programmer’s time to complete a single task
+– integers, separated by a single space.
+
+•	The second line will represent the number of tasks that should be completed
+– integers, separated by a single space.
+
+Output
+•	On the first line, you output:
+o	"Congratulations, all tasks have been completed! Rubber ducks rewarded:"
+•	On the next 4 lines, you output the type and number of ducks given, ordered
+like in the table:
+o	"Darth Vader Ducky: {count}
+Thor Ducky: {count}
+Big Blue Rubber Ducky: {count}
+Small Yellow Rubber Ducky: {count}"
+
+Constraints
+•	All the given numbers will be valid integers in the range [1, 1000].
+•	There will be no negative inputs.
+•	The number of values in both sequences will always be equal.*/
+
+
 public class RubberDuckDebuggers {
     private static final Scanner scanner = new Scanner ( System.in );
 
