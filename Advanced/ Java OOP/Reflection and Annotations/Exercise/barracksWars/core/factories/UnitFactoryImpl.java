edfa8e3a869f48089625a8a7faad7af52364d9a0
Dimitar-Tsanev <Dimitar-Tsanev@users.noreply.github.com>
2024-03-18 15:31:58 +0200

    task 3 done - A New Factory

---
+++ Advanced/ Java OOP/Reflection and Annotations/Exercise/barracksWars/core/factories/UnitFactoryImpl.java
@@ -0,0 +1,18 @@
+package barracksWars.core.factories;
+
+import barracksWars.interfaces.Unit;
+import barracksWars.interfaces.UnitFactory;
+import barracksWars.models.units.AbstractUnit;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class UnitFactoryImpl implements UnitFactory {
+
+	private static final String UNITS_PACKAGE_NAME = "barracksWars.models.units.";
+
+	@Override
+	public Unit createUnit(String unitType) throws  ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+		Class<?> clazz = Class.forName ( UNITS_PACKAGE_NAME + unitType );
+		return  (AbstractUnit) clazz.getDeclaredConstructor (  ).newInstance ( );
+	}
+}
