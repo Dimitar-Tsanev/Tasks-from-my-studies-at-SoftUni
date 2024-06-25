9894444128f8217d70261b778e5474dea6feefe3
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2024-06-25 19:55:45 +0300

    task 3 done

---
+++ JS Front-End/JS Front-End/JS Syntax Fundamentals/Lecture/Excellent Grade.js
@@ -0,0 +1,11 @@
+function isExelentGrade(grade){
+    if (grade >= 5.5){
+        console.log('Excellent');
+
+    }else{
+        console.log('Not excellent');
+
+    }
+}
+isExelentGrade(5.50);
+isExelentGrade(4.35);
