527b94475e3e7a7c18a4e82ece7c81c9c6591158
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2024-06-30 19:43:48 +0300

    task 4 done

---
+++ JS Front-End/JS Front-End/JS Arrays and Strings/Lecture/substring.js
@@ -0,0 +1,5 @@
+function extractSubstring(string, index, numberOfCharacters){
+    console.log(string.substring(index, index + numberOfCharacters));
+}
+extractSubstring('ASentence', 1, 8);
+extractSubstring('SkipWord', 4, 7);
