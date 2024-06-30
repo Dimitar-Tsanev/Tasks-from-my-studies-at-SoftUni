3869e9c66db27e6f8c782befb884da9f347653c2
359883354079 <145838890+Dimitar-Tsanev@users.noreply.github.com>
2024-06-30 18:12:18 +0300

    task 1 done

---
+++ JS Front-End/JS Front-End/JS Arrays and Strings/Lecture/sum_first_and_last_array_elements.js
@@ -0,0 +1,6 @@
+function sumFirstAndLastElements(input){
+    console.log(input[0] + input[input.length - 1]);
+}
+sumFirstAndLastElements([20, 30, 40]);
+sumFirstAndLastElements([10, 17, 22, 33]);
+sumFirstAndLastElements([11, 58, 69]);
